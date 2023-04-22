5dfc13840c8ef830a9185d3389f0276cfd8ee06c
damianayala <damian.villarreal-ayala@menloschool.org>
2023-04-21 22:37:42 -0700

    Final Submission

--- src/BST.java
+++ src/BST.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 /**
  * An Integer Binary Search Tree
- * @author: Your Name Here
- * @version: Date
+ * @author: Damian Villarreal-Ayala
+ * @version: Friday April 21 2023
  */
 
 public class BST {
@@ -35,7 +35,7 @@ public class BST {
      * @param nodes ArrayList of BSTNodes
      */
     public static void printNodes(ArrayList<BSTNode> nodes) {
-        for(int i=0; i<nodes.size()-1; i++) {
+        for(int i=0; i < nodes.size()-1; i++) {
             System.out.print(nodes.get(i) + "-");
         }
         System.out.println(nodes.get(nodes.size()-1));
@@ -48,33 +48,89 @@ public class BST {
      */
     public boolean search(int val) {
         // TODO: Complete the search function
-        return false;
+        //Calling outside function created to recursively look through tree.
+        return helper(root, val);
     }
 
+    //Helper method to recursively find value in tree.
+    public boolean helper(BSTNode currentNode, int value)
+    {
+        if(currentNode == null)
+        {
+            return false;
+        }
+        if(currentNode.getVal() == value)
+        {
+            return true;
+        }
+        if(currentNode.getVal() > value)
+        {
+            return helper(currentNode.getLeft(), value);
+        }
+        if(currentNode.getVal() < value)
+        {
+            return helper(currentNode.getRight(), value);
+        }
+        return false;
+    }
     /**
      * @return ArrayList of BSTNodes in inorder
      */
     public ArrayList<BSTNode> getInorder() {
         // TODO: Complete inorder traversal
-        return null;
+        ArrayList<BSTNode> newArray = new ArrayList<BSTNode>();
+        inOrderArray(root, newArray);
+        return newArray;
     }
 
+    public void inOrderArray(BSTNode current, ArrayList<BSTNode> array)
+    {
+        if(current == null)
+        {
+            return;
+        }
+        inOrderArray(current.getLeft(), array);
+        array.add(current);
+        inOrderArray(current.getRight(), array);
+    }
     /**
      * @return ArrayList of BSTNodes in preorder
      */
     public ArrayList<BSTNode> getPreorder() {
         // TODO: Complete preorder traversal
-        return null;
+        ArrayList<BSTNode> newArray = new ArrayList<BSTNode>();
+        preOrderArray(root, newArray);
+        return newArray;
     }
-
+    public void preOrderArray(BSTNode current, ArrayList<BSTNode> array)
+    {
+        if(current == null)
+        {
+            return;
+        }
+        array.add(current);
+        preOrderArray(current.getLeft(), array);
+        preOrderArray(current.getRight(), array);
+    }
     /**
      * @return ArrayList of BSTNodes in postorder
      */
     public ArrayList<BSTNode> getPostorder() {
         // TODO: Complete postorder traversal
-        return null;
+        ArrayList<BSTNode> newArray = new ArrayList<BSTNode>();
+        postOrderArray(root, newArray);
+        return newArray;
     }
-
+    public void postOrderArray(BSTNode current, ArrayList<BSTNode> array)
+    {
+        if(current == null)
+        {
+            return;
+        }
+        postOrderArray(current.getLeft(), array);
+        postOrderArray(current.getRight(), array);
+        array.add(current);
+    }
     /**
      * Inserts the given integer value to the tree
      * if it does not already exist. Modifies the
@@ -83,6 +139,31 @@ public class BST {
      */
     public void insert(int val) {
         // TODO: Complete insert
+        BSTNode node = new BSTNode(val);
+        if(!search(val))
+        {
+            helpInsert(node, root);
+        }
+    }
+
+    public void helpInsert(BSTNode node, BSTNode current)
+    {
+        if(node.getVal() < current.getVal() && current.getRight() == null)
+        {
+            current.setLeft(node);
+        }
+        else if(node.getVal() > current.getVal() && current.getRight() == null)
+        {
+            current.setRight(node);
+        }
+        if(node.getVal() < current.getVal())
+        {
+            helpInsert(node, current.getLeft());
+        }
+        if(node.getVal() > current.getVal())
+        {
+            helpInsert(node, current.getRight());
+        }
     }
 
     /**
